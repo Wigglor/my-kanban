1b476dc061200ec9e2ca45a17a89c9cffbe9ff1f
Anton Vigstrand <anton.vigstrand@gmail.com>
2019-09-22 15:19:44 +0200

    remove function added

--- app.js
+++ app.js
@@ -11,26 +11,28 @@ UI.prototype.addTask = function(task) {
   let divId = task.stage;
   let prioColor = task.priority;
 
-  console.log(prioColor);
   const output = document.querySelector(`#${divId}-output`);
+  //   const output = document.querySelector(`.${divId} kanban-item`);
   const cardTask = document.createElement("div");
   cardTask.setAttribute("class", `kanban-item-task-${prioColor}`);
+  //   cardTask.setAttribute("class", `kanban-item-task`);
   cardTask.innerHTML = `
+    <i class="fas fa-times"></i>  
     <h2>${task.title}</h2>
     <p>${task.description}</p>
     <p><b>Prio: </b>${task.priority}</p>
     <p><b>Stage: </b>${task.stage}</p>
-    <p class="remove-task">x</p>
     `;
   output.appendChild(cardTask);
   // console.log(cardTask)
 };
 
 //remove a task
-UI.prototype.removeTask = function() {
-  document.querySelector(".remove-task").addEventListener("click", function(e) {
-    console.log(e.target.value);
-  });
+UI.prototype.removeTask = function(target) {
+  //   console.log(target.childNodes[0].childNodes[1].className);
+  if (target.className === "fas fa-times") {
+    target.parentElement.remove();
+  }
 };
 
 document.querySelector("#add-button").addEventListener("click", function(e) {
@@ -39,12 +41,23 @@ document.querySelector("#add-button").addEventListener("click", function(e) {
   const priority = document.querySelector("#priority-input").value;
   const stage = document.querySelector("#stage-input").value;
 
-  // console.log(stage)
   const task = new Task(title, description, priority, stage);
 
   const ui = new UI();
-  if (stage === "doing") {
-    ui.addTask(task);
-  }
+  //   if (stage === "doing") {
+  //     ui.addTask(task);
+  //   }
   ui.addTask(task);
 });
+
+document
+  .querySelector(".kanban-section")
+  .addEventListener("click", function(e) {
+    console.log(e.target);
+    // console.log(e.target.childNodes[0].className);
+    const ui = new UI();
+
+    ui.removeTask(e.target);
+
+    e.preventDefault();
+  });
