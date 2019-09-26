cfc06db230fd8e91d66c7256c92bf68156b0599b
Anton Vigstrand <antonvigstrand@Antons-Air.localdomain>
2019-09-26 17:30:59 +0200

    output in DOM

--- app-module-pattern.js
+++ app-module-pattern.js
@@ -30,7 +30,12 @@ const TaskCtrl = (function(){
       },
       getTasks: function(){
         return data.task;
+      },
+      addTask: function(){
+        const newTask = new Task(title, description, priority, stage)
+        data.task.push(newTask)
       }
+      
     }
   })();
   
@@ -38,10 +43,62 @@ const TaskCtrl = (function(){
   
   // UI Controller
   const UICtrl = (function(){
-    
+      const UISelectors = {
+          btnEl: '#add-button',
+          titleInput: '#title-input',
+          descriptionInput: '#description-input',
+          priorityInput: '#priority-input',
+          stageInput: '#stage-input',
+          todoOutput: '#todo-output'
+      }
+
     // Public methods
     return {
-  
+        populateTaskList: function(tasks){
+            let html = '';
+
+            tasks.forEach(function(task){
+                html+= `
+                <i class="fas fa-times"></i>  
+                <h2>${task.title}</h2>
+                <p>${task.description}</p>
+                <p><b>Prio: </b>${task.priority}</p>
+                <p><b>Stage: </b>${task.stage}</p>
+                <p><b>Points: </b>${task.points}</p>
+                `;
+            });
+
+            document.querySelector(UISelectors.todoOutput).innerHTML = html;
+        },
+        inputTitle: function(){
+            document.querySelector(UISelectors.titleInput).addEventListener("input", function(e){
+                const todoSection = document.querySelector(UISelectors.todoOutput)
+                const output = document.createElement("div");
+                output.innerHTML = e.target.value;
+                todoSection.appendChild(output)
+            })
+        },
+        inputDescription: function(){
+            document.querySelector(UISelectors.descriptionInput).addEventListener("input", function(e){
+                console.log(e.target.value)
+            })
+        },
+        inputPriority: function(){
+            document.querySelector(UISelectors.priorityInput).addEventListener("input", function(e){
+                console.log(e.target.value)
+            })
+        },
+        inputStage: function(){
+            document.querySelector(UISelectors.stageInput).addEventListener("input", function(e){
+                console.log(e.target.value)
+            })
+        },
+        btnClick: function(){
+            document.querySelector(UISelectors.btnEl).addEventListener("click", function(e){
+                console.log(e)
+                // console.log("clicked the button twice")
+            })
+        }
     }
   })();
   
@@ -50,13 +107,27 @@ const TaskCtrl = (function(){
   // App Controller
   const App = (function(TaskCtrl, UICtrl){
     // Public methods
-    const myItems = TaskCtrl.getTasks()
-    // console.log(myItems)
-    console.log(TaskCtrl.logData().task.length)
+
+    // UICtrl.inputTitle();
+    UICtrl.inputDescription();
+    UICtrl.inputPriority();
+    UICtrl.btnClick();
+    UICtrl.inputStage();
+    UICtrl.inputTitle()
+
+    // console.log(TaskCtrl.logData().task)
+    // console.log(TaskCtrl.getTasks())
     return {
       init: function(){
-        console.log('Initializing App...');
-      }
+        // console.log('Initializing App...');
+
+        const tasks = TaskCtrl.getTasks()
+        console.log(tasks)
+
+        UICtrl.populateTaskList(tasks)
+      },
+
+
     }
     
   })(TaskCtrl, UICtrl);
